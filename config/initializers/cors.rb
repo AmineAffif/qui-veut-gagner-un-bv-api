0a5ce78675b80487ce42412cf47b21d82cc0757b
AmineAffif <affif.amine@live.fr>
2024-05-26 19:48:28 +0200

    render deployment

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:3001"
+    origins 'localhost:3001', 'https://qui-veut-gagner-un-bv.vercel.app/'
     resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
