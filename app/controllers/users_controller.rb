bc39cffa49e417b883b762bb137b5394d829be6c
amineaffif <affif.amine@live.com>
2024-06-20 16:22:08 +0200

    refacto

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,13 @@
 class UsersController < ApplicationController
 
   def show
-    p 'current_user =========='
-    p current_user
-    p 'current_user =========='
 
-    user = User.find(params[:id])
-    render json: user, include: :statistic
+    # Check if user is signed in
+    if current_user
+      user = User.find(params[:id])
+      render json: user, include: :statistic
+    else
+      render json: { error: 'Unauthorized' }, status: :unauthorized
+    end
   end
 end
